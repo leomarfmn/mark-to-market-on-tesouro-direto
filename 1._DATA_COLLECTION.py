65b02884bb91dd98bc6b81504ddab7f2f317ea68
Leomar Fonseca de Menezes Neto <leomarfonseca@Leomars-MacBook-Pro.local>
2022-07-27 10:31:12 -0300

    edited file 1: refactored a few lines to get ride of warnings, edited file 2: finished editting formatting the page and formatted the code for readabibility, df: updated

--- 1._DATA_COLLECTION.py
+++ 1._DATA_COLLECTION.py
@@ -1,18 +1,22 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from datetime import date
+import time
 import pandas as pd
 
+from selenium                          import webdriver
+from webdriver_manager.chrome          import ChromeDriverManager
+from selenium.webdriver.support        import expected_conditions as EC
+from selenium.webdriver.common.by      import By
+from selenium.webdriver.support.ui     import WebDriverWait
+from selenium.webdriver.chrome.service import Service
+
 # ACCESSES WEBSITE
-PATH = "/Users/leomarfonseca/Downloads/chromedriver-2"
-driver = webdriver.Chrome(PATH)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.get("https://www.tesourodireto.com.br/titulos/precos-e-taxas.htm")
 
 
 # CLOSES COOKIES
+time.sleep(1)
 WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="onetrust-close-btn-container"]/button'))).click() 
+time.sleep(1)
 WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, 'perfil-notice__close'))).click() 
 
 
@@ -32,7 +36,7 @@ for i in range(len(elements1)):
     df.loc[i, 'VALOR MÍNIMO'] = elements2[i][1]
     df.loc[i, 'VALOR TÍTULO'] = elements2[i][2]
     df.loc[i, 'VENCIMENTO'] = elements2[i][3]
-df['VENCIMENTO'] = pd.to_datetime(df['VENCIMENTO'])
+df['VENCIMENTO'] = pd.to_datetime(df['VENCIMENTO'], format="%d/%m/%Y")
 #print(df.info())
 df.to_csv('df.csv')
     
--- 2._WEB_APPLICATION.py
+++ 2._WEB_APPLICATION.py
@@ -1,26 +1,37 @@
-'''
-1. Add footer:  st.footer('Esse app atualiza os títulos e informações disponíveis uma vez por dia, regularmente.')
-2. Edit information on page 'Teoria'.
-2.1 Add tl;dr section
-2.2 Add one more video
-3. Taxa referencial
-4. Inverter a ordem dos dados na data
-5. Adicionar uma nota de rodapé: Disclaimer: esse não é um site oficial do TD, não tem relação com os vídeos e não deve ser considerado recomendação de investimento. As informações obtidas aqui, devem apenas ser utilizadas como apoio na tomada autônoma de decisão.
-6. Inserir nota de rodapé na página de simulação: os valores calculados não levam em consideração as taxas de custódia cobradas pelo Banco Central, nem as taxas de corretagem possivelmente cobradas pelas corretoras. 
-7. Ajustar largura das dataframes na página
-8. Format page
-9. 10 feriados por ano
-'''
+## TABLE OF CONTENTS
 
-from turtle import width
-from unittest import skip
-import pandas as pd
-import numpy as np
-import streamlit as st
-import datetime
-import plotly.graph_objects as go
+# ------ 1. IMPORTS
+# ------ 2. FUNCTIONS, CONSTANTS AND PAGE SETTINGS
+# ------ 3. SIDEBAR
+# ------ 4. "HOME" PAGE
+# ------ 5. "TEORIA" PAGE
+# ------ 6. "SIMULAÇÃO" PAGE
+    # ------ 6.1. SIDEBAR ON "SIMULAÇÃO" PAGE
+    # ------ 6.2. HEADERS ON "SIMULAÇÃO" PAGE
+    # ------ 6.3. DATE CHECK AND CONTENT CREATION
+        # ------ 6.3.1. SIMULATION VARIABLES
+        # ------ 6.3.2. DATAFRAME VALUES
+        # ------ 6.3.3. TESOURO DIRETO VALUES ON DATAFRAME
+        # ------ 6.3.4. PAGE LAYOUT
+        # ------ 6.3.5. ADJUSTS DATA AND CREATES BARPLOT DATAFRAME
+        # ------ 6.3.6. FIRST COLUMN CONTENT
+        # ------ 6.3.7. CONTAINER 2 CONTENT
+        # ------ 6.3.8. CONTAINER 3 CONTENT
+        # ------ 6.3.9. FOOTER NOTES
+# ------ 7. HIDE STREAMLIT STYLE
+
+
+
+# ------ 1. IMPORTS
+import numpy          as np
+import pandas         as pd
+import streamlit      as st
 import plotly.express as px
+import datetime
 
+
+
+# ------ 2. FUNCTIONS, CONSTANTS AND PAGE SETTINGS
 st.set_page_config(layout="wide")
 
 @st.cache
@@ -40,26 +51,40 @@ def format_date(data):
 def date_diff(a, b):
     return np.busday_count(a, b)
 
+def show_github():
+    with st.sidebar:
+        st.success('Conheça o código fonte:  \n https://github.com/leomarfmn/mark-to-market-on-tesouro-direto')
+    return None
+
 TR = 0.00048
 df = get_data()
 
-# Sidebar
+
+
+# ------ 3. SIDEBAR
 with st.sidebar:
     page = st.radio(
         "Explore",
         ("Home", "Teoria", "Simulações")
     )
 
+
+
+# ------ 4. "HOME" PAGE
 if (page == "Home"):
     st.title('Marcação a mercado no Tesouro Direto')
     st.subheader('Títulos disponíveis* para simulação**')
-    st.write('Escolha uma opção ao lado (Teoria ou Simulações) para mais informações.')
-    st.dataframe(df.set_index('TÍTULO'))
+    st.write('Escolha uma opção ao lado (Teoria ou Simulações) para continuar.')
+    st.table(df.set_index('TÍTULO'))
     st.write('''<p1 style="font-size: small" style="text-align: left;">
                 *Foram listados acima apenas os títulos vendidos direto pelo portal do tesouro direto e com rendimento atrelado ao IPCA ou prefixados.</p1>''',unsafe_allow_html=True)
     st.write('''<p1 style="font-size: small" style="text-align: left;">
                 **Não é recomendação de investimento. A ferramenta deve ser utilizada apenas para cálculos sem compromisso.</p1>''',unsafe_allow_html=True)
+    show_github()
 
+
+
+# ------ 5. "TEORIA" PAGE
 elif (page == "Teoria"):
     st.title('O que é marcação a mercado?')
     st.write('Ainda que os títulos públicos, "prometam" uma taxa de rentabilidade na hora da compra, essas taxas flutuam e, com elas, o valor dos investimentos. Marcação a mercado é a atualização dos valores de um investimento com base nas flutuações das taxas. Em outras palavras, é o valor de resgate do investimento, caso esse seja feito antes do seu vencimento.')
@@ -74,49 +99,63 @@ elif (page == "Teoria"):
     with col2:
         st.video('https://www.youtube.com/watch?v=ll7EB32XM_g')
         st.video('https://www.youtube.com/watch?v=wQqFypK4o4A')
+    show_github()
 
+
+
+# ------ 6. "SIMULAÇÃO" PAGE
 else: 
-    st.title('Simulação')
-    st.subheader('Título escolhido:')
 
+
+    # ------ 6.1. SIDEBAR ON "SIMULAÇÃO" PAGE
     with st.sidebar:
         titulo = st.selectbox('Selecione o título',(df['TÍTULO']))
-
         valor_investido = st.number_input('Insira o valor em reais para a simulação', min_value=0, value=1000)
-
         data_resgate = st.date_input(
-        "Selecione a data de resgate antecipada (ano/mês/dia)",datetime.datetime.today() + datetime.timedelta(365), max_value = datetime.date(2100,12,1))
-
+                                     "Selecione a data de resgate antecipada (ano/mês/dia)",datetime.datetime.today() + datetime.timedelta(365), max_value = datetime.date(2100,12,1))
         selic = st.slider('Escolha a taxa SELIC para o período (%)', 2.0, 18.0, 7.5, step = 0.25)
         ipca = st.slider('Escolha o IPCA médio para o período (%)', 0.0, 15.0, 6.0, step = 0.25)
-        if 'PREFIXADO' in titulo: 
+        if ('PREFIXADO' in titulo): 
             taxa_resgate = st.slider('Escolha a taxa prefixada no momento do resgate', 6.0, 20.0, 11.0, step = 0.25)
         else:
             taxa_resgate = st.slider('Escolha a taxa prefixada no momento do resgate', 2.0, 9.0, 5.0, step = 0.25)
         st.info('Para mais informações sobre a taxa SELIC, visite: https://www.bcb.gov.br/controleinflacao/taxaselic')
         st.info('Para mais informações sobre o IPCA, visite: https://www.bcb.gov.br/controleinflacao/indicepreco')
 
-    df_show = df[df['TÍTULO'] == titulo].set_index('TÍTULO')
-    st.dataframe(df_show)
-    data_vencimento = df[df['TÍTULO'] == titulo]['VENCIMENTO'].tolist()[0]
-    valor_titulo = float(df[df['TÍTULO'] == titulo]['VALOR TÍTULO'].tolist()[0].split(' ')[1].replace('.', '').replace(',','.'))
-    numero_de_titulos = valor_investido / valor_titulo
-    
 
+    # ------ 6.2. HEADERS ON "SIMULAÇÃO" PAGE
+    st.title('Simulação')
+    st.subheader('Título escolhido:')
+    df_show = df[df['TÍTULO'] == titulo].set_index('TÍTULO')
+    st.table(df_show)
+
+
+    # ------ 6.3. DATE CHECK AND CONTENT CREATION
+    data_vencimento = df[df['TÍTULO'] == titulo]['VENCIMENTO'].tolist()[0]
     if (data_resgate > format_date(data_vencimento)):
         st.error("Escolha uma data menor que a data de vencimento do título.")
     else:
 
+        # ------ 6.3.1. SIMULATION VARIABLES
         today = datetime.date.today()
         periodo = pd.date_range(today, data_resgate, freq='D').strftime("%Y-%m-%d").tolist()
         rendimentos = pd.DataFrame({'Data':periodo})
+        valor_titulo = float(df[df['TÍTULO'] == titulo]['VALOR TÍTULO'].tolist()[0].split(' ')[1].replace('.', '').replace(',','.'))
+        numero_de_titulos = valor_investido / valor_titulo
 
+
+        # ------ 6.3.2. DATAFRAME VALUES
+        rendimentos['Intervalo de tempo'] = rendimentos['Data'].apply(lambda x: 
+                                                                      len(pd.date_range(today, x, freq = 'D').tolist())/365)
+        rendimentos['Poupança'] = rendimentos['Intervalo de tempo'].apply(lambda x: 
+                                                                          valor_investido * ((1.005 ** 12 + 0.00048) ** x) if selic >= 8.5 
+                                                                          else valor_investido * ((selic * 0.7) / 100 + 1 + 0.00048) ** x)
+        rendimentos['SELIC'] = rendimentos['Intervalo de tempo'].apply(lambda x: 
+                                                                       valor_investido * calculo_de_rendimento(selic, x))
+        rendimentos['Inflação (IPCA)'] = rendimentos['Intervalo de tempo'].apply(lambda x: 
+                                                                                 valor_investido * calculo_de_rendimento(ipca, x))
         
-        rendimentos['Intervalo de tempo'] = rendimentos['Data'].apply(lambda x: len(pd.date_range(today, x, freq = 'D').tolist())/365)
-        rendimentos['Poupança'] = rendimentos['Intervalo de tempo'].apply(lambda x: valor_investido * ((1.005 ** 12 + 0.00048) ** x) if selic >= 8.5    else valor_investido * ((selic * 0.7) / 100 + 1 + 0.00048) ** x)
-        rendimentos['SELIC'] = rendimentos['Intervalo de tempo'].apply(lambda x: valor_investido * calculo_de_rendimento(selic, x))
-        rendimentos['Inflação (IPCA)'] = rendimentos['Intervalo de tempo'].apply(lambda x: valor_investido * calculo_de_rendimento(ipca, x))
-        
+        # ------ 6.3.3. TESOURO DIRETO VALUES ON DATAFRAME
         if ('PREFIXADO' in titulo):
             taxa_contratada = float(df[df['TÍTULO'] == titulo]['TAXA'].tolist()[0].replace(',','.').replace('%', ''))
             rendimentos['Tesouro direto (teórico)'] = rendimentos['Intervalo de tempo'].apply(lambda x: valor_investido * calculo_de_rendimento(taxa_contratada, x))
@@ -130,20 +169,27 @@ else:
             vna_inicial = valor_titulo / percentual_do_vna_inicial
             rendimentos['Tesouro direto (teórico)'] = rendimentos['Intervalo de tempo'].apply(lambda x: valor_investido * (((ipca / 100 + 1) * (taxa_contratada / 100 + 1)) ** x))
             rendimentos['Tesouro direto (real)'] = rendimentos['Intervalo de tempo'].apply(lambda x: numero_de_titulos * percentual_do_vna_final * vna_inicial * (1 + ipca / 100) ** x)
-        rendimentos.drop(['Intervalo de tempo'], axis = 1 , inplace=True)
 
+        
+        # ------ 6.3.4. PAGE LAYOUT
         container1 = st.container()
         col1, col2 = container1.columns([1, 3])
         container2 = col2.container()
         container3 = col2.container()
         tab1, tab2 = container3.tabs(["📈 Gráfico", "🗃 Dados"])
 
+        
+        # ------ 6.3.5. ADJUSTS DATA AND CREATES BARPLOT DATAFRAME
+        rendimentos.drop(['Intervalo de tempo'], axis = 1 , inplace=True)
         data_resgate = datetime.datetime.strftime(data_resgate, '%Y-%m-%d')
-        df_barplot = rendimentos[rendimentos['Data'] == data_resgate][['Poupança', 'SELIC', 'Inflação (IPCA)', 'Tesouro direto (teórico)', 'Tesouro direto (real)']].astype(str).T.reset_index()
+        df_barplot = rendimentos[rendimentos['Data'] == data_resgate][['Poupança', 'SELIC', 'Inflação (IPCA)', 'Tesouro direto (teórico)', 
+                                                                        'Tesouro direto (real)']].astype(str).T.reset_index()
         df_barplot.columns = ['Comparação','valor']
         df_barplot['valor'] = df_barplot['valor'].apply(lambda x: ((float(x) / valor_investido) - 1 ) * 100)
         df_barplot['valor_str'] = df_barplot['valor'].map('{:,.2f}%'.format)
 
+        
+        # ------ 6.3.6. FIRST COLUMN CONTENT
         with col1:
             data_resgate = data_resgate.split('-')[2] + '/' + data_resgate.split('-')[1] + '/' + data_resgate.split('-')[0]
             st.markdown("<h3 style='text-align: center;'>""</h3>", unsafe_allow_html=True) #Empty line
@@ -179,25 +225,32 @@ else:
                     df_barplot[df_barplot['Comparação'] == 'SELIC']['valor_str'].tolist()[0]), 
                     unsafe_allow_html=True)
 
+        
+        # ------ 6.3.7. CONTAINER 2 CONTENT
         fig = px.line(rendimentos, x='Data', y=['Poupança', 'SELIC', 'Inflação (IPCA)', 'Tesouro direto (teórico)', 'Tesouro direto (real)'], labels = {'Data' : 'Data de resgate', 'value' : 'Valor de resgate  /  Variação (R$)', 'variable' : 'Variável'}, hover_data={'Data' : False})
         fig.update_layout(hovermode="x unified", title = {'text' : 'Variação do retorno com o tempo','xanchor': 'left'})
         container2.plotly_chart(fig, use_container_width=True)
 
+        
+        # ------ 6.3.8. CONTAINER 3 CONTENT
         fig = px.bar(df_barplot, x='Comparação', y = 'valor', text = 'valor_str', labels = {'valor' : 'Rendimento  /  Variação (%)'},hover_data= {'Comparação': False, 'valor' : False, 'valor_str' : False})
         fig.update_layout(title = {'text' : 'Expectativa percentual do retorno','xanchor': 'left'})
         tab1.plotly_chart(fig, use_container_width=True)
         df_show = df_barplot[['Comparação', 'valor_str']].set_index('Comparação')
         df_show.columns = ['Valor']
-        tab2.dataframe(df_show)
+        tab2.table(df_show)
         
+        
+        # ------ 6.3.9. FOOTER NOTES
         st.text("") #Empty line
         st.write('''<p1 style="font-size: small" style="text-align: left;">
                 *Não é recomendação de investimento. A ferramenta deve ser utilizada apenas para cálculos sem compromisso.</p1>''',unsafe_allow_html=True)
         st.write('''<p1 style="font-size: small" style="text-align: left;">
                 **Os resultados mostrados são brutos de imposto de renda e aproximados por conta do cálculo de dias úteis. Além disso, não consideram a taxa de custódia da B3 ou taxas das corretoras.</p1>''',unsafe_allow_html=True)
         
-    
-# ---- HIDE STREAMLIT STYLE
+
+
+# ------ 7. HIDE STREAMLIT STYLE
 hide_st_style = '''
                 <style>
                 #MainMenu {visibility: hidden;}
